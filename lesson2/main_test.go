cabf5ae5a273062d61e811776b02e597888fc89f
petrukhinandrew <dartmol2300@gmail.com>
2023-09-20 13:59:08 +0300

    test: fixed parallel tests

--- lesson2/main_test.go
+++ lesson2/main_test.go
@@ -43,37 +43,35 @@ func TestReplaceSavesBody(t *testing.T) {
 
 }
 
-func TestMultipleGet(t *testing.T) {
-	body := "{'lol': 'kek'}"
-
-	for cnt := 0; cnt < 10; cnt++ {
-		t.Run("go /get", func(t *testing.T) {
-			t.Parallel()
-			goRec := httptest.NewRecorder()
-			goReq := httptest.NewRequest(http.MethodGet, "/get", nil)
-			lesson2.GetHandler(goRec, goReq)
-			if goRec.Result().StatusCode != http.StatusOK {
-				t.Errorf("go /get went wrong on %d", cnt)
-			}
-			if actual := goRec.Body.String(); actual != body {
-				t.Errorf("go /get got %s, expected %s on %d", actual, body, cnt)
-			}
-		})
-	}
-}
-
 func TestMultipleReplace(t *testing.T) {
+
+	TestReplaceSavesBody(t)
+
 	for cnt := 0; cnt < 10; cnt++ {
 
-		body := fmt.Sprintf("body %d", cnt)
+		body, i := fmt.Sprintf("body %d", cnt), cnt
+
 		t.Run("go /replace", func(t *testing.T) {
 			t.Parallel()
+
 			goRec := httptest.NewRecorder()
-			goReq := httptest.NewRequest(http.MethodGet, "/post", strings.NewReader(body))
-			lesson2.GetHandler(goRec, goReq)
+			goReqReplace := httptest.NewRequest(http.MethodPost, "/replace", strings.NewReader(body))
+			goReqGet := httptest.NewRequest(http.MethodGet, "/get", nil)
+
+			lesson2.GetHandler(goRec, goReqReplace)
 
 			if err := goRec.Result().StatusCode; err != http.StatusOK {
-				t.Errorf("go /replace went wrong on %d, got %d", cnt, err)
+				t.Errorf("go /replace went wrong on %d, got %d", i, err)
+			}
+
+			lesson2.GetHandler(goRec, goReqGet)
+
+			if goRec.Result().StatusCode != http.StatusOK {
+				t.Errorf("go /get went wrong on %d", i)
+			}
+
+			if actual := goRec.Body.String(); actual != body {
+				t.Errorf("go /get got %s, expected %s on %d", actual, body, i)
 			}
 		})
 	}
